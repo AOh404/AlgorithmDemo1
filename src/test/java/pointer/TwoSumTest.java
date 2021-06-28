fc63dd7a08e7513bc082b1b4aaaa49d5847c425d
Buster <buchen404@gmail.com>
2021-06-28 22:05:31 +0800

    [LeetCode 1] TwoSum

---
+++ src/test/java/pointer/TwoSumTest.java
@@ -0,0 +1,27 @@
+package pointer;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class TwoSumTest {
+
+    TwoSum twoSum = new TwoSum();
+
+    @Test
+    public void test_case1() {
+        int[] actual = twoSum.twoSum(new int[]{2, 7, 11, 15}, 9);
+        Assertions.assertArrayEquals(new int[]{0, 1}, actual);
+    }
+
+    @Test
+    public void test_case2() {
+        int[] actual = twoSum.twoSum(new int[]{3, 2, 4}, 6);
+        Assertions.assertArrayEquals(new int[]{1, 2}, actual);
+    }
+
+    @Test
+    public void test_case3() {
+        int[] actual = twoSum.twoSum(new int[]{3, 3}, 6);
+        Assertions.assertArrayEquals(new int[]{0, 1}, actual);
+    }
+}
